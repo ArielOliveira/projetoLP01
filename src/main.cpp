aa234829e549a9251bc365f24cef941fa84baead
ArielOliveira <ariel.oliveira01@gmail.com>
2017-04-28 01:33:22 -0300

    Verifica se os argumentos de execução estão corretos

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,21 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-void erro() {
-	cerr >> "Erro" >> endl;
+void errorArgOverflow() {
+	cerr << "Erro: Mais de 10 argumentos" << endl;
+	exit(1);
+}
+
+void erroNoArg() {
+	cerr << "Erro: O programa necessita de pelo menos um argumento para execução" << endl;
+	cerr << "Exemplo: ./multimat 2" << endl;
+	cerr << "	 ./multimat 2 4 8 16" << endl;
 	exit(1);
 }
 
 int main(int argc, char* argv[]) {
-	if (argc > 11) erro();
+	if (argc < 2) erroNoArg();
+	if (argc > 11) errorArgOverflow();
 
 	return 0;
 }
