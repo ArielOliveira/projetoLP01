40f05bd5b72f7b8e8b688fd9974199b04377e351
Hayliiel <rodrigo.oi@hotmail.com>
2017-04-29 03:59:03 +0000

    Término da função recursiva e correção de bugs

--- include/matriz.h
+++ include/matriz.h
@@ -7,8 +7,10 @@ int **MultMatrizesI(int **M1, int **M2, int m);
 
 int **SomaM(int **M1, int **M2, int n);
 
-int **MultMatrizesR(int **M1, int **M2, int **M3, int n);
+int **UnirMatriz(int **M, int **M1, int **M2, int **M3, int **M4, int n);
 
-//int **MultMatrizesR(int **M1, int **M2, int n);
+void AtribuirSubM(int **M, int **M1, int **M2, int **M3, int **M4, int n);
+
+int **MultMatrizesR(int **M1, int **M2, int **M3, int n);
 
 #endif
